14f17e26d4ba0dda898554bae4ff862e20b8ed4b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-24 06:32:01 +0900

    ベクトルの位置情報インタフェースを定義

---
+++ IVectorLocation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPPlusTest
+{
+	/// <summary>
+	/// ベクトル
+	/// </summary>
+	public struct Vector
+	{
+		/// <summary>
+		/// X
+		/// </summary>
+		public int x;
+
+		/// <summary>
+		/// Y
+		/// </summary>
+		public int y;
+	}
+
+	/// <summary>
+	/// ベクトルの位置情報
+	/// </summary>
+	public interface IVectorLocation : ILocation<Vector>
+	{
+		public new Vector Location { get; set; }
+	}
+}
