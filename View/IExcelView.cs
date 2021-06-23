eca23265cae932f36e725497b80ca84301be4f6b
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-24 05:02:39 +0900

    Excelのビューを表すインタフェースをとりあえず定義

---
+++ View/IExcelView.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPPlusTest.View
+{
+	/// <summary>
+	/// Excelのビューインタフェース
+	/// </summary>
+	public interface IExcelView
+	{
+	}
+}
