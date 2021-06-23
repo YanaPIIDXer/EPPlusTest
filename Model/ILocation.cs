6feab6f4910274f5af8420b7d204391bf43dfc05
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-24 04:55:20 +0900

    位置情報を表すインタフェースを定義

---
+++ Model/ILocation.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPPlusTest.Model
+{
+	/// <summary>
+	/// 位置情報インタフェース
+	/// </summary>
+	/// <typeparam name="T">位置情報を表す型</typeparam>
+	public interface ILocation<T>
+	{
+		/// <summary>
+		/// 位置情報
+		/// </summary>
+		T Location { get; set; }
+	}
+}
