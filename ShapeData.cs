b1d6970e3f27fe1613cba10ee126b08603aaf485
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-24 06:33:30 +0900

    図形データクラスをとりあえず定義

---
+++ ShapeData.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPPlusTest
+{
+	/// <summary>
+	/// 図形データ
+	/// </summary>
+	public class ShapeData : IVectorLocation
+	{
+		/// <summary>
+		/// 位置
+		/// </summary>
+		public Vector Location { get; set; }
+
+		/// <summary>
+		/// コンストラクタ
+		/// </summary>
+		public ShapeData()
+		{
+		}
+	}
+}
