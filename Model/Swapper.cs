5bea5c31d2fe59364b1d2ea8f4efd1b040f65c35
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-24 06:17:30 +0900

    簡単なモデルらしきクラスを定義

---
+++ Model/Swapper.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPPlusTest.Model
+{
+	/// <summary>
+	/// 位置のスワップ
+	/// </summary>
+	public class Swapper
+	{
+		/// <summary>
+		/// 位置
+		/// </summary>
+		private IMatrixLocation location = null;
+
+		public Swapper(IMatrixLocation location)
+		{
+			this.location = location;
+		}
+
+		public void Swap()
+		{
+			location.Swap();
+		}
+	}
+
+	/// <summary>
+	/// 拡張メソッドの定義
+	/// </summary>
+	public static class Extensions
+	{
+		/// <summary>
+		/// 行列の入れ替え
+		/// </summary>
+		public static void Swap(this IMatrixLocation matrix)
+		{
+			var newMatrix = new Matrix()
+			{
+				row = matrix.Location.column,
+				column = matrix.Location.row
+			};
+			matrix.Location = newMatrix;
+		}
+	}
+}
