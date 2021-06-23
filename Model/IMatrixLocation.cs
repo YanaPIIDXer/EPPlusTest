95f546611f591261833d516545acb77ec4ba0499
YanaPIIDXer <yana_p_iidxer@yahoo.co.jp>
2021-06-24 05:01:04 +0900

    行列の位置情報インタフェース定義

---
+++ Model/IMatrixLocation.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EPPlusTest.Model
+{
+	/// <summary>
+	/// 行列
+	/// </summary>
+	public struct Matrix
+	{
+		/// <summary>
+		/// 行
+		/// </summary>
+		public int row;
+
+		/// <summary>
+		/// 列
+		/// </summary>
+		public int column;
+	}
+
+	/// <summary>
+	/// 行列の位置情報
+	/// </summary>
+	public interface IMatrixLocation : ILocation<Matrix>
+	{
+		public new Matrix Location { get; set; }
+	}
+}
